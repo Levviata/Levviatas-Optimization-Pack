debbb95a1f7906a6148bd922eb322902cc71a177
Levviata <68709348+Levviata@users.noreply.github.com>
2025-01-21 08:15:47 +0100

    Clean up and add logging

--- scripts/release-all.py
+++ scripts/release-all.py
@@ -1,16 +1,18 @@
 import subprocess
 import json
 
-update_info_path = "update-info.py"
+update_info_path = "../scripts/update-info.py"
 
-set_metadata_path = "set-metadata-all.py"
+set_metadata_path = "../scripts/set-metadata-all.py"
 
-get_modrinth_dependencies_path = "../Modrinth/get-modrinth-dependencies.py"
+get_modrinth_dependencies_path = "../Modrinth/scripts/get-modrinth-dependencies.py"
 
-export_all_path = "export-all.py"
+export_all_path = "../scripts/export-all.py"
 
-release_modrinth_path = "../Modrinth/release-modrinth.py"
-release_curseforge_path = "CurseForge/release-curseforge.py"
+release_modrinth_path = "../Modrinth/scripts/release-modrinth.py"
+release_curseforge_path = "../CurseForge/scripts/release-curseforge.py"
+
+clean_data_path = "../scripts/clean-data.py"
 
 
 def run_script(script_path):
@@ -31,16 +33,20 @@ def run_script(script_path):
         print(f"Script execution failed with error: {e}")
         return e.returncode
 
-# Step 1: Update summary and description of our project in the website
+# Step 1: Update summary and description of our project on the websites
+print("Starting Phase 1")
 run_script(update_info_path)
 
 # Step 2: Set inherited metadata
+print("Starting Phase 2")
 run_script(set_metadata_path)
 
 # Step 3: Get dependencies
+print("Starting Phase 3")
 run_script(get_modrinth_dependencies_path)
 
 # Step 4: Export
+print("Starting Phase 4")
 run_script(export_all_path)
 
 # Check if run, true = prompt user to check files
@@ -49,6 +55,12 @@ with open("../data/has_run.json", "r") as file:
 
 hasRunModrinth = hasRunData["hasRunModrinth"]
 
-
 # Step 5: Release
+print("Starting Phase 5")
 run_script(release_modrinth_path)
+
+# Step 6: Clean data (to avoid using it in case we error out)
+print("Starting Phase 6")
+run_script(clean_data_path)
+
+print("End of script")
